e5af3643f94738ae0eff58c76027852a76c2f4fa
ivan <82367614+ivan3035789@users.noreply.github.com>
2022-09-16 11:23:24 +0300

    finish

--- app/src/main/java/com/example/tetrisproject/GameActivity.kt
+++ app/src/main/java/com/example/tetrisproject/GameActivity.kt
@@ -1,15 +1,22 @@
 package com.example.tetrisproject
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tetrisproject.storage.AppPreferences
+import com.example.tetrisproject.view.TetrisView
+import com.mydomain.tetris.models.AppModel
 
 class GameActivity : AppCompatActivity() {
     var tvHighScore: TextView? = null
     var tvCurrentScore: TextView? = null
+
+    private lateinit var tetrisView: TetrisView
     var appPreferences: AppPreferences? = null
+    private val appModel: AppModel = AppModel()
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +26,52 @@ class GameActivity : AppCompatActivity() {
         val btnRestart = findViewById<Button>(R.id.btn_restart)
         tvHighScore = findViewById<TextView>(R.id.tv_high_score)
         tvCurrentScore = findViewById<TextView>(R.id.tv_current_score)
+        tetrisView = findViewById<TetrisView>(R.id.view_tetris)
+        tetrisView.setActivity(this)
+        tetrisView.setModel(appModel)
+        tetrisView.setOnTouchListener(this::onTetrisViewTouch)
+        btnRestart.setOnClickListener(this::btnRestartClick)
 
         updateHighScore()
         updateCurrentScore()
+    }
+
+    private fun btnRestartClick(view: View) {
+        appModel.restartGame()
+    }
+
+    private fun onTetrisViewTouch(view: View, event: MotionEvent): Boolean {
+        if (appModel.isGameOver() || appModel.isGameAwaitingStart()) {
+            appModel.startGame()
+            tetrisView.setGameCommandWithDelay(AppModel.Motions.DOWN)
+
+        } else if(appModel.isGameActive()) {
+            when (resolveTouchDirection(view, event)) {
+                0 -> moveTetromino(AppModel.Motions.LEFT)
+                1 -> moveTetromino(AppModel.Motions.ROTATE)
+                2 -> moveTetromino(AppModel.Motions.DOWN)
+                3 -> moveTetromino(AppModel.Motions.RIGHT)
+            }
+        }
+        return true
+    }
+
+    private fun resolveTouchDirection(view: View, event: MotionEvent): Int {
+        val x = event.x / view.width
+        val y = event.y / view.height
+        val direction: Int
+
+        direction = if (y > x) {
+            if (x > 1 - y) 2 else 0 } else {
+            if (x > 1 - y) 3 else 1
+        }
+        return direction
+    }
+
+    private fun moveTetromino(motion: AppModel.Motions) {
+        if (appModel.isGameActive()) {
+            tetrisView.setGameCommand(motion)
+        }
     }
 
     private fun updateHighScore() {
